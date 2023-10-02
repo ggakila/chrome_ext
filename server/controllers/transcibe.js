87b24be79d2907ed1ffa5df0cf416bdd9d89ee2c
Erickb <wr.t8r1@gmai.com>
2023-10-02 04:27:42 -0700

    Adding datagram to transcribe audio

--- server/controllers/transcibe.js
+++ server/controllers/transcibe.js
@@ -38,20 +38,38 @@ const sendTranscibeJob = async (sessionId) => {
     const connection = await amqp.connect("amqp://18.119.101.235:15672");
     const channel = await connection.createChannel();
     const queue = "transcribe";
+    const exchangeName = "videoQueue";
+    const routingKey = "softtext";
     const msg = JSON.stringify({sessionId });
     channel.assertQueue(queue, {
       durable: false,
     });
     channel.sendToQueue(queue, Buffer.from(msg));
     console.log(`Message sent to ${queue}`);
+
+    await channel.assertExchange(exchangeName, "direct", { durable: true });
+
+    channel.publish(exchangeName, routingKey,   body=sessionId, {
+    persistent: true,
+    });
+    console.log(`Message sent to ${exchangeName} with routing key ${routingKey}`);
   } catch (error) {
     console.error(error);
   }
 };
 
+async function ffmpeg(command) {
+    return new Promise((resolve, reject) => {
+      exec(`${ffmpegStatic} ${command}`, (err, stderr, stdout) => {
+        if (err) reject(err);
+        resolve(stdout);
+      });
+    });
+  }
+
 
 //converting video to audio using ffmpes
-const convertVideoToAudio = async (sessionId) => {
+const transcribeVideo = async (sessionId) => {
   try {
     const video = await prisma.video.findUnique({
         where: { sessionId: sessionId },
@@ -60,13 +78,73 @@ const convertVideoToAudio = async (sessionId) => {
       throw new Error("Video not found");
     }
     const videoPath = video.url;
+    const videoDirectory = path.dirname(videoPath);
+    const audioPath= videoPath.replace(".webm", "");
+    
+    ffmpeg(`-hide_banner -y -i ${audioPath} ${audioPath}.wav`);
+    const audioFile = {
+        buffer: fs.readFileSync(`${filePath}.wav`),
+        mimetype: "audio/wav",
+    };
 
-    const audioPath = path.join(__dirname, `../uploads/${sessionId}.wav`);
+    const response = await deepgram.transcription.preRecorded(audioFile, {
+        Punctuation: true,
+      });
+    return response.results.channels[0].alternatives[0].transcript;
     
 
   } catch (error) {
     console.error(error);
   }
 };
+
+//consume message from rabbitmq queue
+const consumeTranscribeJob = async () => {
+  try {
+    const connection = await amqp.connect("amqp://18.119.101.235:15672");
+    const channel = await connection.createChannel();
+    const queue = "transcribe";
+    const exchangeName = "videoQueue";
+    const routingKey = "softtext";
+    const msg = JSON.stringify({sessionId });
+    channel.assertQueue(queue, {
+      durable: false,
+    }); 
+    await channel.assertExchange(exchangeName, "direct", { durable: true });
+    await channel.assertQueue(queueName, { durable: true });
+    await channel.bindQueue(queueName, exchangeName, routingKey);
+    channel.consume(queueName, async (msg) => {
+      if (msg !== null) {
+        const videoInfo = JSON.parse(msg.content.toString());
+        let sessionId = videoInfo.id;
+        console.log("Received message from queue:", sessionId);
+        try {
+          transcript = await transcribeVideo(sessionId);
+          console.log("transcript:", transcript);
+          writeATranscript(transcript, sessionId);
+        } catch (error) {
+          console.log(error);
+        }
+        channel.ack(msg);
+      }
+    });
+
+
+  }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const writeATranscript = (transcript, sessionId='non') => {
+    fs.writeFile(`/uploads/${id}.txt`, transcript, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("Transcript file has been written");
+    });
+  };
+
+
 
 

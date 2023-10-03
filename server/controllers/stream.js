a95f296d420402154b8a58711f831197c5885273
Erickb <wr.t8r1@gmai.com>
2023-10-03 00:05:06 -0700

    Stream function to load faster

--- server/controllers/stream.js
+++ server/controllers/stream.js
@@ -126,30 +126,46 @@ const streamvideo = async(req, res) => {
   
     const stat = fs.statSync(videoPath);
     const fileSize = stat.size;
-    const range = req.headers.range;
-  
-    if (range) {
-      const parts = range.replace(/bytes=/, '').split('-');
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-      const chunksize = (end - start) + 1;
-      const file = fs.createReadStream(videoPath, { start, end });
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      };
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      };
-      res.writeHead(200, head);
-      fs.createReadStream(videoPath).pipe(res);
-    }
+    const range = req.headers.range || false ;
+    const videoSize = fs.statSync(videoPath).size
+    const chunkSize = 1 * 1e6;
+    const start = 0;
+    const end = Math.min(start + chunkSize, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+    const stream = fs.createReadStream(videoPath, {
+        start,
+        end
+    })
+    stream.pipe(res);
+    // if (range) {
+    //   const parts = range.replace(/bytes=/, '').split('-');
+    //   const start = parseInt(parts[0], 10);
+    //   const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    //   const chunksize = (end - start) + 1;
+    //   const file = fs.createReadStream(videoPath, { start, end });
+    //   const head = {
+    //     'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+    //     'Accept-Ranges': 'bytes',
+    //     'Content-Length': chunksize,
+    //     'Content-Type': 'video/mp4',
+    //   };
+    //   res.writeHead(206, head);
+    //   file.pipe(res);
+    // } else {
+    //   const head = {
+    //     'Content-Length': fileSize,
+    //     'Content-Type': 'video/mp4',
+    //   };
+    //   res.writeHead(200, head);
+    //   fs.createReadStream(videoPath).pipe(res);
+    // }
 }
 
 
